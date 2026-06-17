c2f9edf16ea72682d40aa986c831e7a0bfdaa563
ayshaakter11 <ayshaakterayshu89@gmail.com>
2026-06-17 05:42:46 +0000

    added patient view function

---
+++ ayshabanu/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    number = 5;
+    printf("%d\n", number);   // এখানে \n add করো
+    printf("asif hossain shuvo\n");
+      printf("asif hossain shuvo\n");
+    return 0;
+}
